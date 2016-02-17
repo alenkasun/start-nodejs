de7fa3d20de508db7ce284c79ab37f339c86abf1
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-17 18:27:16 +0200

    organize work with db

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var User = require('./user');
+var User = require('./user'),
+    db = require('./db');
 
 function run() {
     var user1 = new User("Pete");
     var user2 = new User("Jastine");
 
     user2.hello(user1);
+    console.log(db.getPhrase("Run successful"));
 }
 
 if(module.parent){
