4b021e19cd60e8b81a558dcee862f33857024219
Alyona Shevchenko <fiery.dragon12@gmail.com>
2016-02-19 16:33:43 +0200

    review differences between eventEmitter in nodeJS and event process in browser

---
+++ ee.js
@@ -0,0 +1,22 @@
+// Demo of the simplest usage of EE
+// arguments pass by chain
+// handlers perform in the written order
+
+var EventEmitter = require('events').EventEmitter;
+
+var server = new EventEmitter;
+
+server.on('request', function(request){
+    request.approved = true;
+});
+
+server.on('request', function(request){
+    console.log(request);
+});
+
+server.emit('request', {from: "Client"});
+
+server.emit('request', {from: "Client again"});
+
+
+server.emit('error');  // throw TypeError
--- error.js
+++ error.js
@@ -5,19 +5,49 @@ var phrases = {
     "world": "мир"
 };
 
+
+function PhraseError(status, message) {
+    this.status = status;
+    this.message = message;
+    Error.captureStackTrace(this, PhraseError);
+}
+util.inherits(PhraseError, Error);
+PhraseError.prototype.name = 'PhraseError';
+
+function HttpError(status, message) {
+    this.status = status;
+    this.message = message;
+    Error.captureStackTrace(this, HttpError);
+}
+util.inherits(HttpError, Error);
+HttpError.prototype.name = "HttpError";
+
 function getPhrase(name) {
     if (!phrases[name]) {
-        throw new Error("There is no such phrase as " + name);  // HTTP 500 Internal server error
+        throw new PhraseError(500, "There is no such phrase as " + name);  // HTTP 500 Internal server error
     }
     return phrases[name];
 }
 
 function makePage(url) {
     if(url != 'index.html'){
-        throw new Error("There is no such page!");   // HTTP 404 Not found
+        throw new HttpError(404, "There is no such page!");   // HTTP 404 Not found
     }
     return util.format("%s, %s! ", getPhrase("Hello"), getPhrase("world"));
 }
 
+try {
     var page = makePage('index.html');
     console.log(page);
+} catch (e) {
+    if(e instanceof HttpError){
+        console.log(e.status, e.message);
+    } else {
+        console.log("Error %s\n message: %s\n stack: %s", e.name, e.message, e.stack);
+    }
+}
+
+
+
+
+
