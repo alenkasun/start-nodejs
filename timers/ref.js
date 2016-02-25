88884d5096e0c813270494a70c44d25f3253d519
Alyona Shevchenko <alenka_programmer@mail.ru>
2016-02-25 14:02:12 +0200

    setTimout() doesn't perform until LibUV process all setInterval-request

---
+++ timers/ref.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var server = new http.Server(function(req, res){
+    res.end('temporary enter into the breakpoint');
+}).listen(1337, '127.0.0.1');
+
+
+setTimeout(function(){
+    server.close();
+}, 2500);
+
+setInterval(function(){
+    console.log(process.memoryUsage());
+}, 1000);
+
