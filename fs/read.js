b74d714e3b4630ba681a4535557b40446b908143
Alyona Shevchenko <alenka_programmer@mail.ru>
2016-02-25 18:07:51 +0200

    create, rename and delete file via fs module utilities

--- fs/read.js
+++ fs/read.js
@@ -1,16 +1,14 @@
 var fs = require('fs');
 
-fs.readFile("bla", function(err,data){
-    if(err){
-        if(err.code == 'ENOENT'){
-            console.error(err.message);
-        } else {
-            console.error(err);
-        }
-    } else {
-        console.log(data[0]);
-        console.log(data.length);
-        console.log(__filename);
-        console.log(data.toString());
-    }
+fs.writeFile("file.tmp", "data", function(err){
+    if (err) throw err;
+
+    fs.rename("file.tmp", "new.tmp", function(){
+        if (err) throw err;
+
+        fs.unlink("new.tmp", function(err){
+            if(err) throw err;
+        });
+    });
+
 });
