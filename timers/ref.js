80beaf2fabeb97bcd8fda2fd5dc6aadab9a9d3cb
Alyona Shevchenko <alenka_programmer@mail.ru>
2016-02-25 14:58:06 +0200

    organize code that runs before any additional I/O events

--- timers/ref.js
+++ timers/ref.js
@@ -1,19 +1,11 @@
 var http = require('http');
 
 var server = new http.Server(function(req, res){
-    res.end('temporary enter into the breakpoint');
+
+    process.nextTick(function(){
+        req.on('readable', function(){
+            res.end('use nextTick');
+        });
+    });
+
 }).listen(1337, '127.0.0.1');
-
-
-setTimeout(function(){
-    server.close();
-}, 2500);
-
-var timer = setInterval(function(){
-    console.log(process.memoryUsage());
-}, 1000);
-
-
-// done wather timer isn't important
-timer.unref();
-
