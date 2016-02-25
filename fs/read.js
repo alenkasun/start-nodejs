ada62a539348b9784e0905d9ad99ca0a56965927
Alyona Shevchenko <alenka_programmer@mail.ru>
2016-02-25 17:43:10 +0200

    find out information about errors at https://github.com/joyent/libuv/blob/master/include/uv.h

--- fs/read.js
+++ fs/read.js
@@ -1,11 +1,16 @@
 var fs = require('fs');
 
-fs.readFile(__filename, function(err,data){
+fs.readFile("bla", function(err,data){
     if(err){
-        console.log(err);
+        if(err.code == 'ENOENT'){
+            console.error(err.message);
+        } else {
+            console.error(err);
+        }
     } else {
         console.log(data[0]);
         console.log(data.length);
         console.log(__filename);
+        console.log(data.toString());
     }
 });
