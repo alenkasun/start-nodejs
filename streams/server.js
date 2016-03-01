d3f5f32f95ed57a9f87152a87281c283cb5bafd8
Alyona Shevchenko <alenka_programmer@mail.ru>
2016-03-01 14:08:44 +0200

    calculate one-readable file's fragment

--- streams/server.js
+++ streams/server.js
@@ -5,7 +5,7 @@ var stream = new fs.ReadStream(__filename, {encoding: 'utf-8'});
 
 stream.on('readable', function(){
    var data = stream.read();
-    console.log(data);
+    console.log(data.length);  // длина прочитанного фрагмента
 });
 
 stream.on('end', function () {
