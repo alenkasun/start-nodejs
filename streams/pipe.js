45497bbbde698a6723e0f29ab1efa80b2f3cbd53
Alyona Shevchenko <alenka_programmer@mail.ru>
2016-03-01 15:56:28 +0200

    add process for error handling, open and close writable stream

--- streams/pipe.js
+++ streams/pipe.js
@@ -13,6 +13,23 @@ function sendFile(file, res) {
 
     function write(){
         file.pipe(res);             // output into user
-        file.pipe(process.stdout);  // output into console
+
+        file.on('error', function () {
+           res.statusCode = 500;
+           res.end("Server error");
+           console.log('close');
+        });
+
+        file
+            .on('open', function(){
+                console.log('open');
+            })
+            .on('close', function(){  // нормальное завершение
+                console.log('close');
+            });
+
+        res.on('close', function(){  // сигнал, что соединение было оборвано
+            file.destroy();
+        });
     };
 }
