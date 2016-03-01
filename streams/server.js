d6087a8f513ffe4af409e57eb701ff90dc3220dd
Alyona Shevchenko <alenka_programmer@mail.ru>
2016-03-01 13:58:06 +0200

    output file in string format automatically via encoding

--- streams/server.js
+++ streams/server.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 
 // fs.ReadStream наследует от stream.Readable
-var stream = new fs.ReadStream(__filename);
+var stream = new fs.ReadStream(__filename, {encoding: 'utf-8'});
 
 stream.on('readable', function(){
    var data = stream.read();
-    console.log(data.toString());
+    console.log(data);
 });
 
 stream.on('end', function () {
